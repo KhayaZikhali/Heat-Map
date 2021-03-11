4f02577d68e98576623b6bdbe2bbee2889a0a830
KhayaZikhali <70526289+KhayaZikhali@users.noreply.github.com>
2021-03-11 19:17:05 +0100

    Updated the scss
    commented the javascript code

--- app.js
+++ app.js
@@ -1,90 +1,113 @@
-const api = `https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json`
-      fetch(api)
-           .then(response => {
-             return response.json()
-            })
-            // Shows the tempperature rounded down in degreess
-             .then(data => {
-               console.log(data)})
+var height = 500;
+var width = 1000;
+var margin  = {
+    left:120,
+    right:20,
+    top:20,
+    bottom:70,
+},
+url = `https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json`,
+months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November" ,"December"],
+color = ["#ef5350", "#EC407A", "#AB47BC", " #7E57C2", "#5C6BC0", "#42A5F5" , "#26C6DA", "#26A690" , "#D4E157", "#FFEE58", "#FFA726"]
 
-  const w = 1400;
-    const h = 800;
-    
-    const padding = 50;
-    
-   // Create an x and y scale
-   const xScale = d3.scaleLinear()
-                   .domain([0, d3.max(dataset, (d) => d)])
-                   .range([50, 1.54*h]);
-
-   const yScale = d3.scaleLinear()
-                   .domain([0, d3.max(dataset, (d) => d[1])])
-                   .range([h-padding,padding]);
-
-   const yAxis = d3.axisLeft(yScale);
-   const xAxis = d3.axisBottom(xScale)
-
-  // adjust properties of the body
-const svg = d3.select("body")
-          .append("svg")
-          .attr("width", w)
-          .attr("height", h)
-          .attr("class", "this")
-          
-          
-  // each of the rect elements
-  svg.selectAll("rect")
-     .data(dataset)
-     .enter()
-     .append("rect")
-     .attr("x", (d, i) => {return 50 + i * 4.3}) 
-     .attr("y", (d, i) => {return  h-d[1]/28 -30 })
-     .attr("width", 3)
-     .attr("height", (d, i) => {return d[1]/28})
-     .attr("fill", "blue")
-     .attr("class"," bar ")
-     .append("title")
-     .attr("class", "tooltip tooltiptext") 
-     .text(((d, i ) => "$"+ d[1] + " "+"," + d[0]) )
-     .attr("class" , "title")     
-  // the text above each of the bars     
-  svg.selectAll("text")  
-     .data(dataset)
-     .enter()
-     .append("text")
-     .attr("x", (d, i) =>  (i *100))
-     .attr("y", (d, i) =>  h )
-     .text(  ) 
-     
-     
-  // yAxis label 
-  svg.append("text")
-     .attr("class", "y label")
-     .attr("text-anchor", "end")
-     .attr("y", 60)
-     .attr("dy", ".75em")
-     .attr("transform", "rotate(-90)")
-     .attr("x", -320)
-     .attr("dx", ".75em")
-     .attr("transform", "rotate(-90)")
-     .text("Gross Domestic Product (Billions)");   
-  
-  svg.append("text")
-     .attr("class", "xlabel")
-     .attr("text-anchor", "end")
-     .attr("y", 800)
-     .attr("dy", "0em")
-     .attr("transform", "rotate(-180)")
-     .attr("x", 700)
-     .attr("dx", "0em")
-     .attr("transform", "rotate(0)")
-     .text("Years (1945 - 2015)");   
+// the main canvas 
+var canvas = d3.select("svg").attr({
+    height: height + margin.top + margin.bottom,
+    width : width + margin.left + margin.right
+});
 
 
-  svg.append("g")
-    .attr("transform", "translate("+ (padding  ) + ",20)")
-    .call(yAxis);
+var group = canvas.append("g")
+        .attr({
+          transform: "translate(" + margin.left +","+ margin.top + ")" 
+         })
 
-  svg.append("g")
-     .attr("transform", "translate(0,"+ (h-30) + ")")
-     .call(xAxis) 
+var div = d3.select(".tooltip")
+var xScale = d3.time.scale().range([0, width])
+var yScale = d3.scale.ordinal().domain(months).rangeBands([0, height]);
+var colorScale = d3.scale.quantize().range(color);
+
+
+d3.json(url , (data) => {
+    data = data.monthlyVariance;
+    // map the data to the div
+    data.map((d)=>{
+        d.month = months[d.month-1]
+        //for the y axis 
+        d.year = d3.time.format("%Y").parse(d.year.toString());
+    })
+
+
+// declaring the Xscale 
+xScale.domain(d3.extent(data, (data)=> {
+        return data.year
+}));
+// Colors for the bars 
+
+colorScale.domain(d3.extent(data, (d) => {
+    return d.variance
+}));
+
+// declaring the  width of the bars
+var barWidth = width/ (data.length/12);
+var barHeight = height/ (12);
+
+// declaring the x and y axis 
+var xAxis = d3.svg.axis().scale(xScale).orient("bottom");
+var yAxis = d3.svg.axis().scale(yScale).orient("left").ticks(12);
+
+// rendering the xAxis 
+  group.append('g')
+    .attr({
+      class: 'xAxis',
+      transform: 'translate(0' + (height) + ')'
+    })
+    .call(xAxis)
+
+// rendering the yAxis 
+  group.append('g')
+    .attr({
+      class: 'yAxis',
+      transform: 'translate(0 , 0)'
+    })
+    .call(yAxis)
+ //append g to the svg
+  group.selectAll('g')
+    .data(data)
+    .enter()
+    .append('g')
+    .attr({
+        transform: (data)=> {
+        return "translate(" + xScale(data.year) + " ," + yScale(data.month) + ")";
+        }
+
+    // adding the rectangle 
+    })
+    .append("rect")
+    .attr({
+        width: barWidth,
+        height: yScale.rangeBand()
+    })
+    .style({
+        fill: (data) => {
+            return colorScale(data.variance)
+        }
+    })
+
+    //append the tooltip on mouse hover 
+    .on("mouseover", (d) => {
+        div.transition().duration(10)
+        .style("opacity", 0.8)
+        .style({
+            left: d3.event.pageX + "px",
+            right : d3.event.pageX + "px"
+        })
+       div.html("<p>Year:" + d3.time.format("%Y")(d.year) + " </p> <p>value : " +  (8.66 + d.variance.toFixed(2)) + " </p><p> " + d.month +"</p>")
+    })
+
+    .on("mouseout" , (d)=>{
+        div.transition().duration(100)
+        .style("opacity", 0);
+    })
+
+})
